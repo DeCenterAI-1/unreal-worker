eaaff11eee1e5c3021e955f0e51ee4aa205dbb19
Nasfame <laciferin2024@protonmail.com>
2025-05-26 21:28:05 +0400

    queue : is empty now its possible to exit

--- index.ts
+++ index.ts
@@ -46,14 +46,14 @@ async function processQueue() {
 
     for (const job of data as QueueJob[]) {
 
-      if (job.message.module !== "nearai") {
-      }
+      // if (job.message.module !== "nearai") {
+      //   job.message.module = "nearai" //TODO: remove
+      //   job.message.version = "v0.3.0"
+      //   delete job.message?.inputs?.cpu;
+      //   delete job.message?.inputs?.ram;
+      //   delete job.message?.inputs?.gpu;
+      // }
       
-      job.message.module = "nearai" //TODO: remove
-      job.message.version = "v0.3.0"
-      delete job.message?.inputs?.cpu;
-      delete job.message?.inputs?.ram;
-      delete job.message?.inputs?.gpu;
       
       // job.message = {
       //   ...job.message,
